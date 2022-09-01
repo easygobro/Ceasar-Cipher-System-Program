dcd6b7e55b3a9a1454c5785d4db6dfa19c52cf4c
Sam Ghent <samg@Sams-MacBook-Air.local>
2022-09-01 13:53:46 -0400

    token2 wokring inside encrypt and decrypt, moved main function to top

--- lab1-sghent.c
+++ lab1-sghent.c
@@ -6,98 +6,21 @@
 #include <stdio.h>
 #include <string.h>
 
+//Call funcations
+int Encrypt(char *token2);
+int Decrypt(char *token2);
+int Help();
 
-//avoid implicate error by calling functions before main
-char *token;
-char *token2;
-
-//function to encrypt messages with ceasar cipher || Uses plainText[i] as input and parses through each letter, needs to use token2
-int Encrypt() {
-	
-	char plainText[sizeof(token2)];
-	char cypherText[101];
-	int key = 5;
-	int i;
-
-//	plainText[i] = *token2;
-//- does not work, need to review more about arrays and storing pointers 
-
-// 	ask for user input 	
-//	printf("Please enter some text: ");
-//	fgets(plainText, sizeof(plainText), stdin);
-	
-/*
- *	In order to turn the string into something we can use we must start out by detecting wheather or not it is a capital letter or a lowercase letter.
- *	Then if it is a capital letter that returns a value between 65 and 90, subtract 65 add the key, use mod26 (for 26 letters in the alpabet) and then finally add 65 back 		to the cyphered text.
- *	If it is a lowercase letter do the same but subtract 97, because lowercase letters are between 97 and 122.
- */
-	for (i=0; i < strlen(plainText); i++) {
-		//check if it is a capital letter
-		if(plainText[i]>=65 && plainText[i]<=90) {
-			//loop each letter, encrypt with ceasar cipher
-			cypherText[i]=((((plainText[i]-65))+key)%26)+65;
-			  //check if it a lowercase letter
-		} else if(plainText[i]>=97 && plainText[i]<=122) {
-			//loop each letter, encrypt with ceasar cipher
-			cypherText[i]=((((plainText[i]-97))+key)%26)+97;	
-		} else {
-			cypherText[i] = plainText[i];
-		}	
-	
-	}	
-       		//print out the ciphered text	
-		printf("\nCiphered Text: ");
-		puts(cypherText);	
-		return 0;
-}
-
-int Decrypt() {
-
-	char cypherText[101];
-	char plainText[101];
-	int key = 5;
-	int i;
-
-	printf("Please Enter some ciphered text: ");
-	fgets(cypherText, sizeof(cypherText), stdin);
-
-	for (i=0; i < strlen(cypherText); i++) {
-		//check if it a captial letter
-		if(cypherText[i] >= 65 && cypherText[i]<=90) {
-			//loop each letter, decrypt the ceasar cipher
-			plainText[i]=((((cypherText[i]-65))-key)%26)+65;
-			  //check if it a lowercase letter
-		} else if(cypherText[i]>=97 && cypherText[i]<=122) {
-			//loop each letter, encrypt with ceasar cipher
-			plainText[i]=((((plainText[i]-97))-key)%26)+97;	
-		} else {
-			plainText[i] = cypherText[i];
-		}	
-	}
-
-	//print out the decrypted text
-	printf("\nCiphered Text: ");
-	printf(cypherText, &cypherText);
-	
-	return 0;
-	
-}
-
-
-//Help functoin to keep printf clutter out of the main function
-int Help() { 
-	printf("Welcome to Ceasar Cipher Encrypter/Decrypter\n");
-	printf("Commands avaiable are: encrypt and decrypt\n");
-	printf("syntax must be 'encrypt(message)' otherwise will result in error\n");
-	return 0;
-}
 
 int main() {
-
+	
 	char str[101];
-	//char *token;
-	//char *token2;
+	char *token;
+	char *token2;
+	char result;
+	
 	Help(); //gives a quick list of commands when starting
+		
 	printf("Please enter a command: "); 
 	fgets(str, sizeof(str), stdin); // gether input of command and store it in an array called str
 	
@@ -112,16 +35,74 @@ int main() {
 	//using strcmp (String compare) test is the first token is equal to either encrypt or decrypt.
 	//if strcmp returns 0 then that means the two strings match
 	if (strcmp(token, "encrypt") == 0) {
-		Encrypt(); //if the first token matches, exec Encrypt() --> line 15
+		result = Encrypt(&*token2); //if the first token matches, exec Encrypt() --> line 15
 	} else if (strcmp(token, "decrypt") == 0) {
-		Decrypt(); // if the second token matches, exec Decrypt() --> line 49 
+		result = Decrypt(&*token2); // if the second token matches, exec Decrypt() --> line 49 
 	} else {
 		printf("Please enter a valid command!"); //print out error message if the syntax of tokens does not match either encrypt or decrypt 
 	}
 
 	return 0;
+}
 
+int Encrypt(char *token2) {	
+
+	//Local Variables
+	char cypherText[101];
+	int key = 5;
+	int i;
+
+	//Encryption Loop
+	for (i=0; i < strlen(token2); i++) {
+		//check if it is a capital letter
+		if(token2[i]>=65 && token2[i]<=90) {
+			//loop each letter, encrypt with ceasar cipher
+			cypherText[i]=((((token2[i]-65))+key)%26)+65;
+			  //check if it a lowercase letter
+		} else if(token2[i]>=97 && token2[i]<=122) {
+			//loop each letter, encrypt with ceasar cipher
+			cypherText[i]=((((token2[i]-97))+key)%26)+97;	
+		} else {
+			cypherText[i] = token2[i];
+		}	
+	
 	}
+		printf("Ciphered Text: %s\n", cypherText);
+		return 0;
+}
+
+int Decrypt (char *token2) {
+	//Local Variables
+	char plainText[101];
+	int key = 5;
+	int i;
+	//Decryption loop
+	for (i=0; i < strlen(token2); i++) {
+		//check if it a captial letter
+		if(token2[i] >= 65 && token2[i]<=90) {
+			//loop each letter, decrypt the ceasar cipher
+			plainText[i]=((((token2[i]-65))-key)%26)+65;
+			  //check if it a lowercase letter
+		} else if(token2[i]>=97 && token2[i]<=122) {
+			//loop each letter, encrypt with ceasar cipher
+			plainText[i]=((((token2[i]-97))-key)%26)+97;	
+		} else {
+			plainText[i] = token2[i];
+		}	
+	}
+
+	printf("Deciphered Text: %s\n ", plainText);
+	return 0;
+}
+
+
+int Help() { 
+	printf("**Welcome to Ceasar Cipher Encrypter/Decrypter**\n");
+	printf("--Commands avaiable are: encrypt and decrypt--\n");
+	printf("--syntax must be 'encrypt(message)' otherwise will result in error--\n");
+	return 0;
+}
+
 
 	
 
